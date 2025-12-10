78adb3883fba2834574cac584eeede7551b5a218
josephsaviokav <josephsaviokav@gmail.com>
2025-12-10 14:33:22 +0530

    updated the readme

--- src/main.py
+++ src/main.py
@@ -7,7 +7,7 @@ import numpy as np
 
 
 
-frame = cv2.imread('../assets/tom.jpg')#here replace with tom.jpg if you want to use that image
+frame = cv2.imread('../assets/tom.jpg') #here replace with the image generated using the utils.py program if you want to use that image.
 
 
 
